30aed9a44ed15ee9854098e0edc0a1cc8a7b0c2c
Wiratama Putra Prakosa <wiratama@student.telkomuniversity.ac.id>
2022-05-24 11:54:31 +0700

    [UPDATE] CLB record

--- application/models/M_clbrecord.php
+++ application/models/M_clbrecord.php
@@ -7,10 +7,12 @@
         $this->load->database();
     }
 
-    public function all_data()
+    public function all_data($id)
     {
+        $this->db->where('kd_data_diri', $id);
         $this->db->select('clb.attempt, clb.date, clb.type_of_rest, clb.result, clb.recomendation, clb.follow_up_recomendation,');
         $this->db->from('clb');
+        
         $this->db->order_by('clb.attempt', 'ASC');
         $query = $this->db->get();
         return $query->result();
