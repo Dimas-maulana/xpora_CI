8196967005a900ccddc39a3d713342b70a814695
Fajrul Akram <fajrulakram@gmail.com>
2022-05-30 21:07:28 +0700

    add ekspansi request file

---
+++ application/views/ekspansirequest_detail.php
@@ -0,0 +1,114 @@
+<div class="container" style="background-color: #FFF">
+    <div class="row">
+        <div class="col-12 col-md-6 pt-5">
+            <div class="row">
+                <div class="col-12 d-flex mb-4" style="flex-direction: column">
+                    <label>Request Post ID</label>
+                    <span>
+                        <strong><?= $ekspansi_request->request_post_id ?></strong>
+                    </span>
+                </div>
+                <div class="col-12 d-flex mb-4" style="flex-direction: column">
+                    <label>Nama Komoditas</label>
+                    <span>
+                        <strong><?= $ekspansi_request->comodity_name ?></strong>
+                    </span>
+                </div>
+                <div class="col-12 d-flex mb-4" style="flex-direction: column">
+                    <label>Kategori Komoditas</label>
+                    <span>
+                        <strong><?= $ekspansi_request->category ?></strong>
+                    </span>
+                </div>
+                <div class="col-12 d-flex mb-4" style="flex-direction: column">
+                    <label>Jumlah Request</label>
+                    <span>
+                        <strong><?= $ekspansi_request->qty_komoditas ?></strong>
+                    </span>
+                </div>
+                <div class="col-12 d-flex mb-4" style="flex-direction: column">
+                    <label>Incoterm</label>
+                    <span>
+                        <strong><?= $ekspansi_request->incoterm ?></strong>
+                    </span>
+                </div>
+                <div class="col-12 d-flex mb-4" style="flex-direction: column">
+                    <label>Frekuensi</label>
+                    <span>
+                        <strong><?= $ekspansi_request->frekuensi ?></strong>
+                    </span>
+                </div>
+                <div class="col-12 d-flex mb-4" style="flex-direction: column">
+                    <label>Destinasi</label>
+                    <span>
+                        <strong><?= $ekspansi_request->destination ?></strong>
+                    </span>
+                </div>
+                <div class="col-12 d-flex mb-4" style="flex-direction: column">
+                    <label>Metode Pembayaran</label>
+                    <span>
+                        <strong><?= $ekspansi_request->payment ?></strong>
+                    </span>
+                </div>
+                <div class="col-12 d-flex mb-4" style="flex-direction: column">
+                    <label>Status</label>
+                    <span>
+                        <strong><?= $ekspansi_request->status ?></strong>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 pt-5">
+            <div class="row">
+                <div class="col-12 d-flex mb-4" style="flex-direction: column">
+                    <label>KD Data Diri</label>
+                    <span>
+                        <strong><?= $ekspansi_request->kd_data_diri ?></strong>
+                    </span>
+                </div>
+                <div class="col-12 d-flex mb-4" style="flex-direction: column">
+                    <label>Nama Pemilik</label>
+                    <span>
+                        <strong><?= $ekspansi_request->nama_pemilik ?></strong>
+                    </span>
+                </div>
+                <div class="col-12 d-flex mb-4" style="flex-direction: column">
+                    <label>NIK</label>
+                    <span>
+                        <strong><?= $ekspansi_request->nik ?></strong>
+                    </span>
+                </div>
+                <div class="col-12 d-flex mb-4" style="flex-direction: column">
+                    <label>NPWP</label>
+                    <span>
+                        <strong><?= $ekspansi_request->npwp ?></strong>
+                    </span>
+                </div>
+                <div class="col-12 d-flex mb-4" style="flex-direction: column">
+                    <label>Nomor Telepon</label>
+                    <span>
+                        <strong><?= $ekspansi_request->no_telp ?></strong>
+                    </span>
+                </div>
+                <div class="col-12 d-flex mb-4" style="flex-direction: column">
+                    <label>Email</label>
+                    <span>
+                        <strong><?= $ekspansi_request->email ?></strong>
+                    </span>
+                </div>
+                <div class="col-12 d-flex mb-4" style="flex-direction: column">
+                    <label>Domisili</label>
+                    <span>
+                        <strong><?= $ekspansi_request->domisili ?></strong>
+                    </span>
+                </div>
+                <div class="col-12 d-flex mb-4" style="flex-direction: column">
+                    <label>Tipe User</label>
+                    <span>
+                        <strong><?= $ekspansi_request->user_type ?></strong>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
